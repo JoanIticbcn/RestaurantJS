ecd8bfe110e22fbcb029971c4b93e02de78f733a
Joan <joan@joan.com>
2024-05-08 16:27:13 +0200

    Onscroll

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ var video = document.getElementById('vid');
 
 function playvideo(){
     video.play();
+    document.getElementById('play').innerHTML='pause_circle';
 }
 
 document.getElementById('play').addEventListener('click', ()=> {
     if(video.paused){
         video.play();
-        document.getElementById('play').innerHTML='play_circle';
+        document.getElementById('play').innerHTML='pause_circle';
     }else{
         video.pause();
-        document.getElementById('play').innerHTML='pause_circle';
+        document.getElementById('play').innerHTML='play_circle';
     }
 });
 
 document.getElementById('vup').addEventListener('click', ()=> {
-    video.muted = false;
-});
-
-document.getElementById('vof').addEventListener('click', ()=> {
-    video.muted = true;
+    if(video.muted===true){
+        video.muted = false;
+        document.getElementById('vup').innerHTML='volume_up';
+    }else{
+        video.muted = true;
+        document.getElementById('vup').innerHTML='volume_off';
+    }
+    
 });
